3627a21c27e0e20971970e52ac7619e78f4ff4de
davidlucas443 <davidga19391@gmail.com>
2026-04-30 11:04:22 -0300

    conf: configurar app.json e arquivo principal App.js

--- App.js
+++ App.js
@@ -1,20 +1,13 @@
+import React from 'react';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { RootNavigator } from './src/routes/RootNavigator';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaProvider>
+      <RootNavigator />
+      <StatusBar barStyle="dark-content" />
+    </SafeAreaProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
